f2d70f565e7680bcd7c78ca4d4e74d1ee25d1213
Olabode Oyinkansola Qudus <generalfocus1@gmail.com>
2024-01-04 13:00:39 -0800

    Update aws.ts

--- udagram-api-user/src/aws.ts
+++ udagram-api-user/src/aws.ts
@@ -1,35 +1,44 @@
-import AWS = require('aws-sdk');
-import {config} from './config/config';
+import AWS from 'aws-sdk';
 
+// Create AWS SDK instance
+const awsConfig = {
+  region: process.env.AWS_REGION,
+  credentials: new AWS.SharedIniFileCredentials({ profile: process.env.AWS_PROFILE }),
+};
 
-// Configure AWS
-const credentials = new AWS.SharedIniFileCredentials({profile: config.aws_profile});
-AWS.config.credentials = credentials;
+AWS.config.update(awsConfig);
 
-export const s3 = new AWS.S3({
+const s3 = new AWS.S3({
   signatureVersion: 'v4',
-  region: config.aws_region,
-  params: {Bucket: config.aws_media_bucket},
+  params: { Bucket: process.env.AWS_MEDIA_BUCKET },
 });
 
-// Generates an AWS signed URL for retrieving objects
-export function getGetSignedUrl( key: string ): string {
-  const signedUrlExpireSeconds = 60 * 5;
+export const getGetSignedUrl = async (key: string, expireSeconds = 300): Promise<string> => {
+  try {
+    const signedUrl = await s3.getSignedUrlPromise('getObject', {
+      Bucket: process.env.AWS_MEDIA_BUCKET,
+      Key: key,
+      Expires: expireSeconds,
+    });
+    return signedUrl;
+  } catch (error) {
+    // Handle errors appropriately
+    console.error('Error generating signed GET URL:', error);
+    throw error;
+  }
+};
 
-  return s3.getSignedUrl('getObject', {
-    Bucket: config.aws_media_bucket,
-    Key: key,
-    Expires: signedUrlExpireSeconds,
-  });
-}
-
-// Generates an AWS signed URL for uploading objects
-export function getPutSignedUrl( key: string ): string {
-  const signedUrlExpireSeconds = 60 * 5;
-
-  return s3.getSignedUrl('putObject', {
-    Bucket: config.aws_media_bucket,
-    Key: key,
-    Expires: signedUrlExpireSeconds,
-  });
-}
+export const getPutSignedUrl = async (key: string, expireSeconds = 300): Promise<string> => {
+  try {
+    const signedUrl = await s3.getSignedUrlPromise('putObject', {
+      Bucket: process.env.AWS_MEDIA_BUCKET,
+      Key: key,
+      Expires: expireSeconds,
+    });
+    return signedUrl;
+  } catch (error) {
+    // Handle errors appropriately
+    console.error('Error generating signed PUT URL:', error);
+    throw error;
+  }
+};
